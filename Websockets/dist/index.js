4ab2e930c91fa9b54104fd7c40b98b2255227347
Saikat-D023 <saikatdey358@gmail.com>
2025-01-28 20:51:31 +0530

    ping pong be

--- Websockets/dist/index.js
+++ Websockets/dist/index.js
@@ -3,9 +3,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
 //event handler
+// wss.on("connection", function(socket){
+//     console.log("user connected");
+//     setInterval(()=>{
+//         socket.send("Curr price of solana is"+ Math.random())
+//     }, 500)
+//     socket.on("message", (e) => {
+//         console.log(e.toString());
+//     })
+// })
+//ping-pong backend
 wss.on("connection", function (socket) {
     console.log("user connected");
-    setInterval(() => {
-        socket.send("Curr price of solana is" + Math.random());
-    }, 500);
+    socket.on("message", (e) => {
+        if (e.toString() == "ping") {
+            socket.send("pong");
+        }
+    });
 });
--- Websockets/src/index.ts
+++ Websockets/src/index.ts
@@ -4,16 +4,27 @@ const wss = new WebSocketServer({ port: 8080 });
 
 
 //event handler
-wss.on("connection", function(socket){
+// wss.on("connection", function(socket){
 
-    console.log("user connected");
-    setInterval(()=>{
-        socket.send("Curr price of solana is"+ Math.random())
-    }, 500)
+//     console.log("user connected");
+//     setInterval(()=>{
+//         socket.send("Curr price of solana is"+ Math.random())
+//     }, 500)
+
+//     socket.on("message", (e) => {
+//         console.log(e.toString());
+//     })
+// })
+
+
+//ping-pong backend
+wss.on("connection", function(socket){
+    console.log("user connected")
 
     socket.on("message", (e) => {
-        console.log(e.toString());
+        if(e.toString() == "ping"){
+            socket.send("pong");
+        }
     })
 })
-
   
