174b3ab0492ca528dca8eb2f68575207a72551bf
Saikat-D023 <saikatdey358@gmail.com>
2025-01-28 10:10:03 +0530

    connection established and response received

--- Websockets/src/index.ts
+++ Websockets/src/index.ts
@@ -1,1 +1,19 @@
-console.log("hi")
+import {WebSocketServer} from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+
+//event handler
+wss.on("connection", function(socket){
+
+    console.log("user connected");
+    setInterval(()=>{
+        socket.send("Curr price of solana is"+ Math.random())
+    }, 500)
+
+    socket.on("message", (e) => {
+        console.log(e.toString());
+    })
+})
+
+  
