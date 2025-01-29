78493d03e787d7a5024d0fcbbcdc87ddac0eda35
Saikat-D023 <saikatdey358@gmail.com>
2025-01-29 23:27:01 +0530

    both event handler added

--- Websockets/src/index.ts
+++ Websockets/src/index.ts
@@ -4,17 +4,17 @@ const wss = new WebSocketServer({ port: 8080 });
 
 
 //event handler
-// wss.on("connection", function(socket){
+wss.on("connection", function(socket){
 
-//     console.log("user connected");
-//     setInterval(()=>{
-//         socket.send("Curr price of solana is"+ Math.random())
-//     }, 500)
+    console.log("user connected");
+    setInterval(()=>{
+        socket.send("Curr price of solana is"+ Math.random())
+    }, 500)
 
-//     socket.on("message", (e) => {
-//         console.log(e.toString());
-//     })
-// })
+    socket.on("message", (e) => {
+        console.log(e.toString());
+    })
+})
 
 
 //ping-pong backend
